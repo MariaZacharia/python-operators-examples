534394154802a83054a6e2f09fa37dc860efc109
Maria Zacharia <sgmzacha@liverpool.ac.uk>
2026-02-06 14:57:20 +0200

    Add PEP 8 compliant basic operators examples

---
+++ basic_operators.py
@@ -0,0 +1,52 @@
+# Basic Operators Examples (PEP 8 compliant)
+
+# Arithmetic Operators
+# Example 1:
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# Example 2:
+remainder = 11 % 3
+print(remainder)
+
+# Example 3:
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed) 
+
+# Using Operators with Strings
+# Example 4:
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+# Example 5:
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+# Using Operators with Lists
+# Example 6:
+even_numbers = [2, 4, 6, 8]
+odd_numbers = [1, 3, 5, 7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+# Example 7:
+print([1, 2, 3] * 3)
+
+# Exercise:
+x = 3
+y = 5
+
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!") 

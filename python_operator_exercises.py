d2128cb6bf18a1c1a2f7f4bb7e59e7c62de0d394
Maria Zacharia <sgmzacha@liverpool.ac.uk>
2026-02-06 16:56:29 +0200

    Add PEP 8 compliant exercise 18 examples

---
+++ python_operator_exercises.py
@@ -0,0 +1,15 @@
+# Exercise 18-a: Assignment Operator =
+lst = ["yellow", "white", "blue"]
+print(lst)
+
+# Exercise 18-b: Division Operator /
+a = 10
+b = 3
+result = a / b
+print(result)   
+
+# Exercise 18-c: Addition using +=
+vertical_speed = 750
+vertical_speed += 100
+print(vertical_speed) 
+
